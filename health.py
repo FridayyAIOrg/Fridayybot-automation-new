c2c0ce1638b09ba90bab631a57f8c8a7b73a479f
Varad J <varad.gcs@gmail.com>
2025-09-09 00:46:44 +0530

    Show version of current bot in health check endpoint for debugging

--- health.py
+++ health.py
@@ -1,7 +1,7 @@
 from aiohttp import web
 
 async def health_check(request):
-    return web.Response(text="OK")
+    return web.Response(text="OK, v-1.0.0")
 
 def create_health_app():
     app = web.Application()
